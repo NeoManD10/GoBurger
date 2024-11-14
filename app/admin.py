bb9864e02df02b8735d3a32870a45dc6fb26b0f3
Mistakensito <megustaelbisteque@gmail.com>
2024-11-14 00:59:51 -0300

    Añadida funcionalidad de administrar ingredientes

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Ingrediente
 
+admin.site.register(Ingrediente)
 # Register your models here.
+class Ingrediente(admin.ModelAdmin):
+    mostrar_lista = ('nombre', 'tipo', 'precio', 'disponible')
